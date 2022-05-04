023d2676df0f3fe6b93f7ba71b8f468e14912b95
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-05-05 01:39:50 +0530

    Time: 0 ms (100.00%), Space: 6 MB (88.88%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.length()<=1)
+        {
+            return 1;
+        }
+        if( isupper(word[0]) && isupper(word[1]) ) {
+            for(int i=2;i<word.length();i++){
+                if(islower(word[i])){
+                    return 0;
+                }
+            }
+        }
+        else{for(int i=1;i<word.length();i++){
+                if(isupper(word[i])){
+                    return 0;
+                }
+        }
+    }
+            return 1;
+    }
+};
