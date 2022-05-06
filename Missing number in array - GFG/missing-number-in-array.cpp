d22b4dccf1be172a6d9b313ca59b9b98dc12f964
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-05-06 22:55:15 +0530

    Added solution - LeetHub

--- Missing number in array - GFG/missing-number-in-array.cpp
+++ Missing number in array - GFG/missing-number-in-array.cpp
@@ -13,10 +13,9 @@ using namespace std;
 class Solution{
   public:
     int MissingNumber(vector<int>& array, int n) {
-       int sum=0;
-    sum=n*(n+1)/2;
+       int sum=n*(n+1)/2;
        for(int i=0;i<n-1;i++){
-           sum=sum-array[i];
+           sum-=array[i];
        }
        return sum;
     }
