c2d796720f3d7a03ba3f483eb7fffc828de94852
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-06-04 01:13:57 +0530

    Time: 11 ms (28.61%), Space: 7.2 MB (98.32%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,19 +1,45 @@
+// class Solution {
+// public:
+//     // Brute Force Approach
+//     // Time - O(N)
+//     // Space - O(N)    
+//     bool isPalindrome(string s) {
+//         string f="";
+//         for(int i=0;i<s.length();i++){
+//             if(isalnum(s[i])){
+//                 f+=tolower(s[i]);
+//             }
+//         }
+//         string l="";
+//         for(int i=s.length()-1;i>=0;i--){
+//             if(isalnum(s[i])){
+//                 l+=tolower(s[i]);
+//             }
+//         }
+//         return f==l;
+//     }
+// };
+
+// Optimised 
+
+   // Time - O(LogN)
+   // Space - O(1)    
+
 class Solution {
-public:
+    public :
     bool isPalindrome(string s) {
-        string f="";
-        for(int i=0;i<s.length();i++){
-            if(isalnum(s[i])){
-                f+=tolower(s[i]);
-            }
-        }
-        string l="";
-        for(int i=s.length()-1;i>=0;i--){
-            if(isalnum(s[i])){
-                l+=tolower(s[i]);
-            }
-        }
-        cout<<f<<" "<<l;
-        return f==l;
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--) { 
+        while (isalnum(s[i]) == false && i < j){
+           i++; 
+        }  
+        while (isalnum(s[j]) == false && i < j){
+           j--; 
+        } 
+        if (tolower(s[i]) != tolower(s[j])){
+           return false;  
+        } 
     }
+    
+    return true;
+  }
 };
