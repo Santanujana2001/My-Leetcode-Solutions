7f18c9c09d605cba52c767aec616017a3e786a64
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-06-11 23:36:04 +0530

    Added solution - LeetHub

---
+++ Magnet Array Problem - GFG/magnet-array-problem.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    double solve(double l, double h, double magnets[],int n){
+        double mid;
+        while(l<h){
+            mid = (l+h)/2;
+            double force = 0;
+            for(int i=0;i<n; i++){
+                force+= 1/(mid-magnets[i]); 
+            }
+            if(abs(force)<0.0000001) return mid;
+            else if(force<0){
+                h = mid;
+            }
+            else{
+                l = mid;
+            }
+        }
+        return mid;
+    }
+        void nullPoints(int n, double magnets[], double getAnswer[])
+    {
+        for(int i=0;i<n-1;i++){
+            getAnswer[i] = solve(magnets[i],magnets[i+1],magnets,n);
+        }  
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        double magnets[n], getAnswer[n];
+        for(int i=0;i<n;i++)
+        cin>>magnets[i];
+        Solution ob;
+        ob.nullPoints(n, magnets, getAnswer);
+        
+        for(int i=0; i<n-1; i++)
+            printf("%0.2lf ",getAnswer[i]);
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
