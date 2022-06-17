2d88e7084a938f96398857b04bd03da6f39b6e75
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-06-17 19:20:02 +0530

    Time: 22 ms (11.26%), Space: 10.1 MB (43.20%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -5,8 +5,8 @@ public:
         int yd=(c[1][1]-c[0][1]);
         int curx,cury;
         for(int i=2;i<c.size();i++){
-            curx=(c[i][0]-c[0][0]);
-            cury=(c[i][1]-c[0][1]);
+            curx=(c[i][0]-c[i-1][0]);
+            cury=(c[i][1]-c[i-1][1]);
             if(xd*cury!=yd*curx){
                 return false;
             }
