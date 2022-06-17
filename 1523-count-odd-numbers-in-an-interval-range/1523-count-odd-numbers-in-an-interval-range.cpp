1ccb5d7c714bb06899bccae321425fa7976ec67d
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-06-17 23:59:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.79%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int countOdds(int low, int high) {
         int odds=(high-low)/2;
+//         If Low And High Both Are Even Then Return The Ans 
         if(low%2==0 && high%2==0){
             return odds;
         }
+//         Otherwise Increment The Ans And Return 
         return ++odds;
     }
 };
