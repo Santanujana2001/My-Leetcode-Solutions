b33f34224b62ef567025bdc02c7c2bb53338f9a1
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-06-24 01:33:21 +0530

    Added solution - LeetHub

---
+++ Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int row, int column) 
+    {
+        int t=0,b=matrix.size()-1,l=0,r=matrix[0].size()-1,dir=0,i;
+        vector<int>ans;
+        while(t<=b && l<=r){
+            if(dir==0){
+                for(i=l;i<=r;i++){
+                    ans.push_back(matrix[t][i]);
+                }
+                t++;
+            }
+            else if(dir==1){
+                for(i=t;i<=b;i++){
+                    ans.push_back(matrix[i][r]);
+                }
+                r--;
+            }
+            else if(dir==2){
+                for(i=r;i>=l;i--){
+                    ans.push_back(matrix[b][i]);
+                }
+                b--;
+            }
+            else if(dir==3){
+                for(i=b;i>=t;i--){
+                    ans.push_back(matrix[i][l]);
+                }
+                l++;
+            }
+            dir=(dir+1)%4;
+        }
+        return ans; 
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int r,c;
+        cin>>r>>c;
+        vector<vector<int> > matrix(r); 
+
+        for(int i=0; i<r; i++)
+        {
+            matrix[i].assign(c, 0);
+            for( int j=0; j<c; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
