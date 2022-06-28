cc09f57fd52620fa90d614504abf2ee30c81f979
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-06-28 11:22:52 +0530

    Time: 229 ms (21.96%), Space: 17.2 MB (6.31%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -3,17 +3,14 @@ public:
     vector<int> v;
     NumArray(vector<int>& nums) {
         for(int i:nums){
-            // cout<<i<<" ";
             v.push_back(i);
         }
     }
-    
     int sumRange(int left, int right) {
         int s=accumulate(v.begin()+left,v.begin()+right+1,0);
         return s;
     }
 };
-
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
