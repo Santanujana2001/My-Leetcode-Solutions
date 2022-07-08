65737eef00bfe21413378d6b2c3133bb43b32fda
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-07-08 23:08:42 +0530

    Time: 8 ms (58.22%), Space: 16.4 MB (78.36%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -1,30 +1,36 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-//     kevin naughton's explaination is really good
-    bool ans(TreeNode* l,TreeNode* r){
-        if(!l || !r){
-            return l==r;
+class Solution
+{
+    public:
+       	//     kevin naughton's explaination is really good
+        bool ans(TreeNode *l, TreeNode *r)
+        {
+            if (!l || !r)
+            {
+                return l == r;
+            }
+            if (l->val != r->val)
+            {
+                return false;
+            }
+            return ans(l->left, r->right) && ans(l->right, r->left);
         }
-        if(l->val!=r->val){
-            return false;
-        }
-        return ans(l->left,r->right) && ans(l->right,r->left);
-    }
-    bool isSymmetric(TreeNode* root) {
-        if(!root){
+    bool isSymmetric(TreeNode *root)
+    {
+        if (!root)
+        {
             return true;
         }
-        return ans(root->left,root->right);
+        return ans(root->left, root->right);
     }
 };
