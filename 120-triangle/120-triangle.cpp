65bedaaebca595022dd51b54f91d57f01106614b
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-07-10 01:03:28 +0530

    Time: 15 ms (18.98%), Space: 8.6 MB (65.41%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int> > &triangle) {
+    int n = triangle.size();
+    vector<int> mini(triangle[n-1]);
+    for (int layer = n-2; layer >= 0; layer--)
+    {
+        for (int i = 0; i <= layer; i++) 
+        {
+            mini[i] = min(mini[i], mini[i+1]) + triangle[layer][i]; 
+        }
+    }
+    return mini[0];
+}
+};
