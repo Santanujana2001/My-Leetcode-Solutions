65b433b924e70e4272de30b408a7802a22538f75
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-07-22 22:51:03 +0530

    Time: 15 ms (18.01%), Space: 17.3 MB (12.11%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void solve(vector<int>& nums,set<vector<int>>&ans,int ind,vector<int>vec){
-        if(ind>=nums.size()){
+        if(ind==nums.size()){
             ans.insert(vec);
             return ;
         }
@@ -9,17 +9,17 @@ public:
         solve(nums,ans,ind+1,vec);
 //         include part
         vec.push_back(nums[ind]);
-        sort(vec.begin(),vec.end());
+        sort(vec.begin(),vec.end()); // 3rd change - sort the vector to avoid the duplicates which are unsorted
         solve(nums,ans,ind+1,vec);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>>ans;
-        vector<vector<int>>v;
+        set<vector<int>>ans; // 1st change - take set of vectors to avoid collecting duplicates 
+        vector<vector<int>>v; // 2nd change - now this is the answer vectors
         vector<int>vec;
         int ind=0;
         solve(nums,ans,ind,vec);
         for(auto i:ans){
-            v.push_back(i);
+            v.push_back(i); // Copying The Elements From The Set To Vector Of Vectors
         }
         return v;
     }
