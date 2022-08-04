f9b568548cd4e80bf84471f05af0e046dd80dacb
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-08-05 01:20:24 +0530

    Time: 3 ms (58.76%), Space: 8.1 MB (19.58%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+    public:
+        int solve(vector<int> v)
+        {
+            int prev1 = 0, prev2 = 0;
+            for (int i = 0; i < v.size(); i++)
+            {
+                int take = v[i];
+                if (i > 1)
+                {
+                    take += prev2;
+                }
+                int nottake = prev1;
+                int cur = max(take, nottake);
+                prev2 = prev1;
+                prev1 = cur;
+            }
+            return prev1;
+        }
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+        {
+            return nums[0];
+        }
+        vector<int> first, last;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i != 0)
+            {
+                last.push_back(nums[i]);
+            }
+            if (i != n - 1)
+            {
+                first.push_back(nums[i]);
+            }
+        }
+        return max(solve(last), solve(first));
+    }
+};
