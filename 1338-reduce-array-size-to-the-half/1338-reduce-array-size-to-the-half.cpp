280a28798473dd76d14428f090018add5d96c319
Santanu Jana <92827304+Santanujana2001@users.noreply.github.com>
2022-08-19 23:39:03 +0530

    Time: 151 ms (99.31%), Space: 58 MB (96.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,27 +1,33 @@
-class Solution {
-public:
-//     learnt from farz's yt video
-    int minSetSize(vector<int>& arr) {
-        vector<int>v;
-        sort(arr.begin(),arr.end());
-        int count=1;
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]==arr[i-1]){
+class Solution
+{
+    public:
+       	//     learnt from farz's yt video
+        int minSetSize(vector<int> &arr)
+        {
+            vector<int> v;
+            sort(arr.begin(), arr.end());
+            int count = 1;
+            for (int i = 1; i < arr.size(); i++)
+            {
+                if (arr[i] == arr[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    v.push_back(count);
+                    count = 1;
+                }
+            }
+            v.push_back(count);
+            count = 0;
+            sort(v.begin(), v.end());
+            int n = arr.size() / 2;
+            for (int i = v.size() - 1; i >= 0 && n > 0; i--)
+            {
                 count++;
+                n -= v[i];
             }
-            else{
-                v.push_back(count);
-                count=1;
-            }
+            return count;
         }
-        v.push_back(count);
-        count=0;
-        sort(v.begin(),v.end());
-        int n=arr.size()/2;
-        for(int i=v.size()-1;i>=0 && n>0 ;i--){
-            count++;
-            n-=v[i];
-        }
-        return count;
-    }
 };
